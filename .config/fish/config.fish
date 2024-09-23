feccb22905a1a75e4704e6564e4e9fd757c9e6f9
Aryan <75756768+aynp@users.noreply.github.com>
2024-09-23 08:04:29 +0530

    minor tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,26 @@ if status is-interactive
   pfetch
 end
 
+# Functions needed for !! and !$
+function history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
 # The bindings for !! and !$
 if [ "$fish_key_bindings" = "fish_vi_key_bindings" ];
   bind -Minsert ! history_previous_command
--- .profile
+++ .profile
@@ -1,3 +1,18 @@
 # Added by Toolbox App
 export PATH="$PATH:/home/aryan/.local/share/JetBrains/Toolbox/scripts:/home/aryan/go/bin"
 
+# KUBECONFIG
+export KUBECONFIG="$HOME/.kube/config"
+
+# Go environment variables
+export GOPATH="$HOME/go"
+export GOBIN="$GOPATH/bin"
+
+# Set default editor to nvim
+export EDITOR="nvim"
+
+export PNPM_HOME="$HOME/.local/share/pnpm"
+export CARGO_BIN="$HOME/.cargo/bin"
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$PNPM_HOME:$CARGO_BIN:$BUN_INSTALL/bin:$PATH"
+
