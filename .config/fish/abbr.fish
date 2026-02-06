0821357428e88a339efc60754e9fc297f85b45a6
Aryan Pathania <aryan@mercari.com>
2026-02-07 07:15:22 +0900

    bruh, only one command is enough

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -4,9 +4,6 @@ abbr -a -- e nvim
 abbr -a -- k kubectl
 abbr -a -- g gcloud 
 
-abbr -a -- x exit # imported from a universal variable, see `help abbr`
-abbr -a -- d docker # imported from a universal variable, see `help abbr`
-abbr -a -- e nvim # imported from a universal variable, see `help abbr`
-abbr -a -- k kubectl # imported from a universal variable, see `help abbr`
-
 abbr -a -- gc git checkout 
+
+abbr -a -- gcl gcloud auth login --update-adc
--- .config/fish/functions/glogin.fish
+++
@@ -1,6 +0,0 @@
-function glogin
-    echo "Logging in to gcloud..."
-    gcloud auth login &>/dev/null
-    and echo "Logging in to application-default..."
-    and gcloud auth application-default login &>/dev/null
-end
